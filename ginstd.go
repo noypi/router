58d9bd8b2c5d7bef5f0a08d2a9cd168dde33cd20
noypi <adrian.migraso@gmail.com>
2017-12-12 09:22:56 +0800

    add func(context), func(interface{}) handlers

--- ginstd.go
+++ ginstd.go
@@ -8,7 +8,39 @@ import (
 
 type IRoutesStd interface {
 	UseH(...http.Handler) IRoutesStd
-	Use(...http.HandlerFunc) IRoutesStd
+	UseF(...http.HandlerFunc) IRoutesStd
+	UseC(...Handler) IRoutesStd
+	Use(...interface{}) IRoutesStd
+
+	Handle(string, string, ...interface{}) IRoutesStd
+	Any(string, ...interface{}) IRoutesStd
+	GET(string, ...interface{}) IRoutesStd
+	POST(string, ...interface{}) IRoutesStd
+	DELETE(string, ...interface{}) IRoutesStd
+	PATCH(string, ...interface{}) IRoutesStd
+	PUT(string, ...interface{}) IRoutesStd
+	OPTIONS(string, ...interface{}) IRoutesStd
+	HEAD(string, ...interface{}) IRoutesStd
+
+	HandleC(string, string, ...Handler) IRoutesStd
+	AnyC(string, ...Handler) IRoutesStd
+	GETC(string, ...Handler) IRoutesStd
+	POSTC(string, ...Handler) IRoutesStd
+	DELETEC(string, ...Handler) IRoutesStd
+	PATCHC(string, ...Handler) IRoutesStd
+	PUTC(string, ...Handler) IRoutesStd
+	OPTIONSC(string, ...Handler) IRoutesStd
+	HEADC(string, ...Handler) IRoutesStd
+
+	HandleF(string, string, ...http.HandlerFunc) IRoutesStd
+	AnyF(string, ...http.HandlerFunc) IRoutesStd
+	GETF(string, ...http.HandlerFunc) IRoutesStd
+	POSTF(string, ...http.HandlerFunc) IRoutesStd
+	DELETEF(string, ...http.HandlerFunc) IRoutesStd
+	PATCHF(string, ...http.HandlerFunc) IRoutesStd
+	PUTF(string, ...http.HandlerFunc) IRoutesStd
+	OPTIONSF(string, ...http.HandlerFunc) IRoutesStd
+	HEADF(string, ...http.HandlerFunc) IRoutesStd
 
 	HandleH(string, string, ...http.Handler) IRoutesStd
 	AnyH(string, ...http.Handler) IRoutesStd
@@ -19,16 +51,6 @@ type IRoutesStd interface {
 	PUTH(string, ...http.Handler) IRoutesStd
 	OPTIONSH(string, ...http.Handler) IRoutesStd
 	HEADH(string, ...http.Handler) IRoutesStd
-
-	Handle(string, string, ...http.HandlerFunc) IRoutesStd
-	Any(string, ...http.HandlerFunc) IRoutesStd
-	GET(string, ...http.HandlerFunc) IRoutesStd
-	POST(string, ...http.HandlerFunc) IRoutesStd
-	DELETE(string, ...http.HandlerFunc) IRoutesStd
-	PATCH(string, ...http.HandlerFunc) IRoutesStd
-	PUT(string, ...http.HandlerFunc) IRoutesStd
-	OPTIONS(string, ...http.HandlerFunc) IRoutesStd
-	HEAD(string, ...http.HandlerFunc) IRoutesStd
 }
 
 //type IRoutes = gin.IRoutes
@@ -50,6 +72,98 @@ func (e *EngineStd) Gin() *gin.Engine {
 	return e.Engine
 }
 
+// func (interface{})
+func (e *EngineStd) Handle(httpMethod, relativePath string, handlers ...interface{}) IRoutesStd {
+	e.Engine.Handle(httpMethod, relativePath, Wrap(handlers...)...)
+	return e
+}
+
+func (e *EngineStd) Any(relativePath string, handlers ...interface{}) IRoutesStd {
+	wrapMethod(e.Engine.Any, relativePath, handlers)
+	return e
+}
+
+func (e *EngineStd) GET(relativePath string, handlers ...interface{}) IRoutesStd {
+	wrapMethod(e.Engine.GET, relativePath, handlers)
+	return e
+}
+
+func (e *EngineStd) POST(relativePath string, handlers ...interface{}) IRoutesStd {
+	wrapMethod(e.Engine.POST, relativePath, handlers)
+	return e
+}
+
+func (e *EngineStd) DELETE(relativePath string, handlers ...interface{}) IRoutesStd {
+	wrapMethod(e.Engine.DELETE, relativePath, handlers)
+	return e
+}
+
+func (e *EngineStd) PATCH(relativePath string, handlers ...interface{}) IRoutesStd {
+	wrapMethod(e.Engine.PATCH, relativePath, handlers)
+	return e
+}
+
+func (e *EngineStd) PUT(relativePath string, handlers ...interface{}) IRoutesStd {
+	wrapMethod(e.Engine.PUT, relativePath, handlers)
+	return e
+}
+
+func (e *EngineStd) OPTIONS(relativePath string, handlers ...interface{}) IRoutesStd {
+	wrapMethod(e.Engine.OPTIONS, relativePath, handlers)
+	return e
+}
+
+func (e *EngineStd) HEAD(relativePath string, handlers ...interface{}) IRoutesStd {
+	wrapMethod(e.Engine.HEAD, relativePath, handlers)
+	return e
+}
+
+// func(context.Context)
+func (e *EngineStd) HandleC(httpMethod, relativePath string, handlers ...Handler) IRoutesStd {
+	e.Engine.Handle(httpMethod, relativePath, WrapC(handlers...)...)
+	return e
+}
+
+func (e *EngineStd) AnyC(relativePath string, handlers ...Handler) IRoutesStd {
+	wrapMethodC(e.Engine.Any, relativePath, handlers)
+	return e
+}
+
+func (e *EngineStd) GETC(relativePath string, handlers ...Handler) IRoutesStd {
+	wrapMethodC(e.Engine.GET, relativePath, handlers)
+	return e
+}
+
+func (e *EngineStd) POSTC(relativePath string, handlers ...Handler) IRoutesStd {
+	wrapMethodC(e.Engine.POST, relativePath, handlers)
+	return e
+}
+
+func (e *EngineStd) DELETEC(relativePath string, handlers ...Handler) IRoutesStd {
+	wrapMethodC(e.Engine.DELETE, relativePath, handlers)
+	return e
+}
+
+func (e *EngineStd) PATCHC(relativePath string, handlers ...Handler) IRoutesStd {
+	wrapMethodC(e.Engine.PATCH, relativePath, handlers)
+	return e
+}
+
+func (e *EngineStd) PUTC(relativePath string, handlers ...Handler) IRoutesStd {
+	wrapMethodC(e.Engine.PUT, relativePath, handlers)
+	return e
+}
+
+func (e *EngineStd) OPTIONSC(relativePath string, handlers ...Handler) IRoutesStd {
+	wrapMethodC(e.Engine.OPTIONS, relativePath, handlers)
+	return e
+}
+
+func (e *EngineStd) HEADC(relativePath string, handlers ...Handler) IRoutesStd {
+	wrapMethodC(e.Engine.HEAD, relativePath, handlers)
+	return e
+}
+
 // with method
 func (e *EngineStd) NoMethodH(handlers ...http.Handler) {
 	e.Engine.NoMethod(WrapH(handlers...)...)
@@ -65,7 +179,7 @@ func (e *EngineStd) HandleH(httpMethod, relativePath string, handlers ...http.Ha
 	return e
 }
 
-func (e *EngineStd) Handle(httpMethod, relativePath string, handlers ...http.HandlerFunc) IRoutesStd {
+func (e *EngineStd) HandleF(httpMethod, relativePath string, handlers ...http.HandlerFunc) IRoutesStd {
 	e.Engine.Handle(httpMethod, relativePath, WrapF(handlers...)...)
 	return e
 }
@@ -75,7 +189,7 @@ func (e *EngineStd) AnyH(relativePath string, handlers ...http.Handler) IRoutesS
 	return e
 }
 
-func (e *EngineStd) Any(relativePath string, handlers ...http.HandlerFunc) IRoutesStd {
+func (e *EngineStd) AnyF(relativePath string, handlers ...http.HandlerFunc) IRoutesStd {
 	wrapMethodF(e.Engine.Any, relativePath, handlers)
 	return e
 }
@@ -85,7 +199,7 @@ func (e *EngineStd) GETH(relativePath string, handlers ...http.Handler) IRoutesS
 	return e
 }
 
-func (e *EngineStd) GET(relativePath string, handlers ...http.HandlerFunc) IRoutesStd {
+func (e *EngineStd) GETF(relativePath string, handlers ...http.HandlerFunc) IRoutesStd {
 	wrapMethodF(e.Engine.GET, relativePath, handlers)
 	return e
 }
@@ -95,7 +209,7 @@ func (e *EngineStd) POSTH(relativePath string, handlers ...http.Handler) IRoutes
 	return e
 }
 
-func (e *EngineStd) POST(relativePath string, handlers ...http.HandlerFunc) IRoutesStd {
+func (e *EngineStd) POSTF(relativePath string, handlers ...http.HandlerFunc) IRoutesStd {
 	wrapMethodF(e.Engine.POST, relativePath, handlers)
 	return e
 }
@@ -105,7 +219,7 @@ func (e *EngineStd) DELETEH(relativePath string, handlers ...http.Handler) IRout
 	return e
 }
 
-func (e *EngineStd) DELETE(relativePath string, handlers ...http.HandlerFunc) IRoutesStd {
+func (e *EngineStd) DELETEF(relativePath string, handlers ...http.HandlerFunc) IRoutesStd {
 	wrapMethodF(e.Engine.DELETE, relativePath, handlers)
 	return e
 }
@@ -115,7 +229,7 @@ func (e *EngineStd) PATCHH(relativePath string, handlers ...http.Handler) IRoute
 	return e
 }
 
-func (e *EngineStd) PATCH(relativePath string, handlers ...http.HandlerFunc) IRoutesStd {
+func (e *EngineStd) PATCHF(relativePath string, handlers ...http.HandlerFunc) IRoutesStd {
 	wrapMethodF(e.Engine.PATCH, relativePath, handlers)
 	return e
 }
@@ -125,7 +239,7 @@ func (e *EngineStd) PUTH(relativePath string, handlers ...http.Handler) IRoutesS
 	return e
 }
 
-func (e *EngineStd) PUT(relativePath string, handlers ...http.HandlerFunc) IRoutesStd {
+func (e *EngineStd) PUTF(relativePath string, handlers ...http.HandlerFunc) IRoutesStd {
 	wrapMethodF(e.Engine.PUT, relativePath, handlers)
 	return e
 }
@@ -135,7 +249,7 @@ func (e *EngineStd) OPTIONSH(relativePath string, handlers ...http.Handler) IRou
 	return e
 }
 
-func (e *EngineStd) OPTIONS(relativePath string, handlers ...http.HandlerFunc) IRoutesStd {
+func (e *EngineStd) OPTIONSF(relativePath string, handlers ...http.HandlerFunc) IRoutesStd {
 	wrapMethodF(e.Engine.OPTIONS, relativePath, handlers)
 	return e
 }
@@ -145,7 +259,7 @@ func (e *EngineStd) HEADH(relativePath string, handlers ...http.Handler) IRoutes
 	return e
 }
 
-func (e *EngineStd) HEAD(relativePath string, handlers ...http.HandlerFunc) IRoutesStd {
+func (e *EngineStd) HEADF(relativePath string, handlers ...http.HandlerFunc) IRoutesStd {
 	wrapMethodF(e.Engine.HEAD, relativePath, handlers)
 	return e
 }
@@ -157,7 +271,17 @@ func (e *EngineStd) UseH(handlers ...http.Handler) IRoutesStd {
 	return e
 }
 
-func (e *EngineStd) Use(handlers ...http.HandlerFunc) IRoutesStd {
+func (e *EngineStd) UseF(handlers ...http.HandlerFunc) IRoutesStd {
 	wrapF(e.Engine.Use, handlers)
+	return e
+}
+
+func (e *EngineStd) UseC(handlers ...Handler) IRoutesStd {
+	wrapC(e.Engine.Use, handlers)
+	return e
+}
+
+func (e *EngineStd) Use(handlers ...interface{}) IRoutesStd {
+	wrap(e.Engine.Use, handlers)
 	return e
 }
--- wrap.go
+++ wrap.go
@@ -7,10 +7,12 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Handler func(context.Context)
+
 func WrapF(handlers ...http.HandlerFunc) []gin.HandlerFunc {
 	ginh := make([]gin.HandlerFunc, len(handlers))
 	for i, h := range handlers {
-		ginh[i] = ginWrapF(h)
+		ginh[i] = GinWrapF(h)
 	}
 	return ginh
 }
@@ -18,7 +20,23 @@ func WrapF(handlers ...http.HandlerFunc) []gin.HandlerFunc {
 func WrapH(handlers ...http.Handler) []gin.HandlerFunc {
 	ginh := make([]gin.HandlerFunc, len(handlers))
 	for i, h := range handlers {
-		ginh[i] = ginWrapH(h)
+		ginh[i] = GinWrapH(h)
+	}
+	return ginh
+}
+
+func WrapC(handlers ...Handler) []gin.HandlerFunc {
+	ginh := make([]gin.HandlerFunc, len(handlers))
+	for i, h := range handlers {
+		ginh[i] = GinWrapC(h)
+	}
+	return ginh
+}
+
+func Wrap(handlers ...interface{}) []gin.HandlerFunc {
+	ginh := make([]gin.HandlerFunc, len(handlers))
+	for i, h := range handlers {
+		ginh[i] = GinWrap(h)
 	}
 	return ginh
 }
@@ -31,13 +49,32 @@ func GetWriter(ctx context.Context) http.ResponseWriter {
 	return ctx.(*gin.Context).Writer
 }
 
-func ginWrapF(f http.HandlerFunc) gin.HandlerFunc {
+func GinWrap(f interface{}) gin.HandlerFunc {
+	switch fn := f.(type) {
+	case Handler:
+		return GinWrapC(fn)
+	case http.HandlerFunc:
+		return GinWrapF(fn)
+	case http.Handler:
+		return GinWrapH(fn)
+	default:
+		panic("unsupported handler type")
+	}
+}
+
+func GinWrapC(f Handler) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		f(c)
+	}
+}
+
+func GinWrapF(f http.HandlerFunc) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		f(NewContextW(c), c.Request)
 	}
 }
 
-func ginWrapH(h http.Handler) gin.HandlerFunc {
+func GinWrapH(h http.Handler) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		h.ServeHTTP(NewContextW(c), c.Request)
 	}
@@ -49,6 +86,22 @@ func wrapH(fn func(handlers ...gin.HandlerFunc) gin.IRoutes, handlers []http.Han
 
 func wrapF(fn func(handlers ...gin.HandlerFunc) gin.IRoutes, handlers []http.HandlerFunc) {
 	fn(WrapF(handlers...)...)
+}
+
+func wrapC(fn func(handlers ...gin.HandlerFunc) gin.IRoutes, handlers []Handler) {
+	fn(WrapC(handlers...)...)
+}
+
+func wrap(fn func(handlers ...gin.HandlerFunc) gin.IRoutes, handlers []interface{}) {
+	fn(Wrap(handlers...)...)
+}
+
+func wrapMethod(fn func(relativePath string, handlers ...gin.HandlerFunc) gin.IRoutes, relativePath string, handlers []interface{}) {
+	fn(relativePath, Wrap(handlers...)...)
+}
+
+func wrapMethodC(fn func(relativePath string, handlers ...gin.HandlerFunc) gin.IRoutes, relativePath string, handlers []Handler) {
+	fn(relativePath, WrapC(handlers...)...)
 }
 
 func wrapMethodH(fn func(relativePath string, handlers ...gin.HandlerFunc) gin.IRoutes, relativePath string, handlers []http.Handler) {
